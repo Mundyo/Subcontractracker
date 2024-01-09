86d49456d3ff16e9c2523403f6d7054db6899ffc
Le-Yo <deoleyo@gmail.com>
2024-01-09 12:36:09 -0600

    cors4

--- backend/server.js
+++ backend/server.js
@@ -18,18 +18,22 @@ const PORT = 3001;
 // const allowedOrigins = ['http://localhost:3000', 'https://subcontractracker-095b11d35777.herokuapp.com'];
 const allowedOrigins = ['http://localhost:3000', 'https://orderissuetracker-19f85e06e952.herokuapp.com', 'https://orderissuetracker.com'];
 
+// app.use(cors({
+//   origin: function (origin, callback) {
+   
+//     if (!origin) return callback(null, true);
+
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       const msg = 'The CORS policy for this site does not allow access from the specified origin.';
+//       return callback(new Error(msg), false);
+//     }
+
+//     return callback(null, true);
+//   },
+//   credentials: true,
+// }));
 app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified origin.';
-      return callback(new Error(msg), false);
-    }
-
-    return callback(null, true);
-  },
+  origin: true,
   credentials: true,
 }));
 
